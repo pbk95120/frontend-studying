99aa06113c4743759924e0e2351ee50e42a8534c
Park <pbk95120@gmail.com>
2023-06-18 00:56:08 +0900

    6/18

---
+++ VueStudy/vuedongsan/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <OneRoomModal
+    @closeModal="modalOn = false"
+    :onerooms="onerooms"
+    :clickNum="clickNum"
+    :modalOn="modalOn"
+  />
+  <ItemCard
+    @openModal="
+      modalOn = true;
+      clickNum = $event;
+    "
+    :onerooms="onerooms"
+    :clickNum="clickNum"
+    :modalOn="modalOn"
+  />
+  <DiscountBanner />
+  <!-- <div v-for="(a, i) in products" :key="i">
+    <img :src="require('./assets/room' + i + '.jpg')" class="room-img" />
+    <h4 @click="modalOn = true">{{ products[i] }}</h4>
+    <p>{{ prices[i] }}</p>
+    <button @click="increase(i)">허위매물신고</button>
+    <div>신고수 : {{ report[i] }}</div>
+  </div> -->
+</template>
+
+<script>
+import data from "./data.js";
+import DiscountBanner from "./components/DiscountBanner.vue";
+import OneRoomModal from "./components/OneRoomModal.vue";
+import ItemCard from "./components/ItemCard.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      clickNum: 0,
+      onerooms: data,
+      menus: ["Home", "Shop", "About"],
+      prices: [60, 70, 80],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      report: [0, 0, 0],
+      modalOn: false,
+    };
+  },
+  methods: {
+    increase(num) {
+      this.report[num] += 1;
+    },
+  },
+  components: {
+    DiscountBanner,
+    ItemCard,
+    OneRoomModal,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+</style>
---
+++ VueStudy/vuedongsan/src/components/OneRoomModal.vue
@@ -0,0 +1,39 @@
+/
+<template>
+  <div class="black-bg" v-if="modalOn === true">
+    <div class="white-bg">
+      <h4>{{ onerooms[clickNum].title }}</h4>
+      <p>{{ onerooms[clickNum].content }}</p>
+      <p>{{ onerooms[clickNum].price }}</p>
+      <button @click="$emit('closeModal', i)">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OneRoomModal",
+  props: {
+    onerooms: Array,
+    modalOn: Boolean,
+    clickNum: Number,
+  },
+};
+</script>
+
+<style>
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
