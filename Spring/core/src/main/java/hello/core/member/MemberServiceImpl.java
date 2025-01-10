fc2fe073d516edebdffe7b5b5f543d55be9200db
Park <pbk95120@gmail.com>
2025-01-10 17:43:57 +0900

    01/10

--- Spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -8,11 +8,18 @@ public class MemberServiceImpl implements MemberService {
         this.memberRepository = memberRepository;
     }
 
+    @Override
     public void join(Member member) {
         memberRepository.save(member);
     }
 
+    @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
