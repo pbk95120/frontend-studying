da8300c1178bc3216ee593ae7fdad6fbf2df7af1
pbk95120 <pbk95120@plateer.com>
2025-11-17 11:15:24 +0900

    11/17

--- Bundling/example-project/emoji.js
+++
@@ -1,8 +0,0 @@
-const emojis = [
-  { icon: '😊', name: 'Smiling Face' },
-  { icon: '🚀', name: 'Rocket' },
-  { icon: '🍕', name: 'Pizza' },
-  { icon: '🐱', name: 'Cat' },
-  { icon: '🌈', name: 'Rainbow' },
-  { icon: '🎸', name: 'Guitar' }
-];
--- Bundling/example-project/webpack.config.js
+++ Bundling/example-project/webpack.config.js
@@ -2,9 +2,21 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./main.js", // 어떤 파일을 진입점으로 번들링할지
+  entry: "./main.ts", // 웹팩이 읽기 시작할 파일을 .ts로 변경했어요.
   output: {
-    filename: "bundle.js", // 번들로 만들어질 파일 이름
-    path: path.resolve(__dirname, "dist"), // 번들 파일이 어디에 저장될지
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // .ts 파일들은
+        use: "ts-loader", // ts-loader를 거쳐 처리돼요.
+        exclude: /node_modules/, // 외부 모듈은 제외해요.
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"], // 파일을 import할 때 확장자를 생략할 수 있어요. TypeScript와 JavaScript를 혼용하는 프로젝트에서 설정해두면 좋아요.
   },
 };
